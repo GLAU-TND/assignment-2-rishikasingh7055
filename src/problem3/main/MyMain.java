e0f721d579d8106633fdb6457ab155beaf760c31
Ritik1306 <ritik.kaushik06@gmail.com>
2020-03-30 16:16:02 +0530

    creating main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,20 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+	public static void main(String[] args) {
+		MyPriorityQueue queue = new MyPriorityQueue();
+		queue.enqueue(new Student("harsh",58));
+		queue.enqueue(new Student("harshit",23));
+		queue.enqueue(new Student("harshita",65));
+		queue.enqueue(new Student("ritik",1));
+		queue.enqueue(new Student("rishika",10));
+		queue.enqueue(new Student("mehul",76));
+
+		queue.show();
+		queue.dequeue();
+		queue.show();
+	}
 }
+
+
+
